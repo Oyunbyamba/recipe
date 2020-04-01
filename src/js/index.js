8fa592b3b501c5c4e8725b614672b2adf22ad241
oyunbyamba <oyunbyamba@gmail.com>
2020-04-01 10:45:22 +0800

    state-обьект, search controller - нэмлээ

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,30 @@
-import axios from "axios";
 import Search from "./model/search";
 
-let search = new Search("pasta");
-search.doSerach().then(r => console.log(r));
+/**
+ * Web app төлөв / state /
+ *
+ * - Хайлтын query, үр дүн
+ * - Тухайн үзүүлж байгаа жор
+ * - Лайкалсан жоруудын жагсаалт
+ * - Захиалж байгаа жорын найрлагууд
+ */
+const state = {};
+const controlSearch = async () => {
+  // 1. Вэбээс хайлтын түлхүүр үгийг гаргаж авна. /Хайлт хийсэн утга авна - "pizza" гм/
+  const query = "pizza";
+
+  if (query) {
+    // 2. Түлхүүр үгээр хайдаг Search()-обьектыг үүсгэж өгнө. /Шинээр хайлтын обьектыг үүсгэнэ/
+    state.search = new Search(query);
+
+    // 3. Хайлт хийхэд зориулж дэлгэцийг бэлтгэнэ /UI/. /Хайлтын үр дүнг харуулах хэсгийг цэвэрлэж бэлтгэнэ/
+    // 4. Хайлтыг гүйцэтгэнэ.
+    await state.search.doSearch();
+    // 5. Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+    console.log(state.search.result);
+  }
+};
+document.querySelector(".search").addEventListener("submit", e => {
+  e.preventDefault(); // Submit автоматаар хийгдэх үйлдлийг болиулна. /Пост хийгдэхгүй/
+  controlSearch();
+});
--- src/js/model/search.js
+++ src/js/model/search.js
@@ -5,7 +5,7 @@ export default class Search {
   constructor(query) {
     this.query = query;
   }
-  async doSerach() {
+  async doSearch() {
     try {
       let result = await axios(
         "https://forkify-api.herokuapp.com/api/search?q=" + this.query
